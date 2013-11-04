7d09db266149a5be5dcee20a0e609bd5151001f8
Nicholas Flintham <nick@flinny.org>
2013-11-04 16:35:47 +0000

    Re-add build barriers
    Change-Id: I531300d723aab8d8e038db36802cd4950480a4b6

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
 ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(TARGET_USES_CM_POWERHAL),true)
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+
 LOCAL_PATH := $(call my-dir)
 
 # HAL module implemenation stored in
@@ -14,4 +17,6 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+endif
+endif
 endif
